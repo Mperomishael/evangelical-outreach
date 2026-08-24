d2ce3cbcf735c2293613cff293791168291a1c60
Mishael Yakubu <147824425+Mperomishael@users.noreply.github.com>
2026-08-24 04:16:45 -1100

    Refactor Layout component to remove settings usage
    Removed settings dependency and updated layout structure.

--- otega-v2/src/components/Layout.tsx
+++ otega-v2/src/components/Layout.tsx
@@ -2,18 +2,16 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import WhatsAppFloat from './WhatsAppFloat';
-import { useSettings } from '../hooks/useSettings';
 
 export default function Layout() {
-  const { settings } = useSettings();
   return (
-    <div className="min-h-screen flex flex-col bg-black">
-      <Navbar settings={settings} />
-      <main className="flex-1">
-        <Outlet context={{ settings }} />
+    <div className="min-h-screen bg-[#E3F2FD] flex flex-col">
+      <Navbar />
+      <main className="flex-1 py-8 px-4">
+        <Outlet />
       </main>
-      <Footer settings={settings} />
-      <WhatsAppFloat number={settings.whatsapp_number} />
+      <Footer />
+      <WhatsAppFloat />
     </div>
   );
 }
